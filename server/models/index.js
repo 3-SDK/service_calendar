ebf330711738c1bdb9cc143f3886e1a5400ed146
Kang Jie Tan <kangjietan@gmail.com>
2020-02-12 12:50:16 -0800

    Query db from model

--- server/models/index.js
+++ server/models/index.js
@@ -2,11 +2,19 @@ const db = require('../db/cassandra.js');
 
 module.exports = {
   get: (callback) => {
+    // queryStr
+    // db.query
   },
   post: (callback) => {
+    // queryStr
+    // db.query
   },
   put: (callback) => {
+    // queryStr
+    // db.query
   },
   delete: (callback) => {
+    // queryStr
+    // db.query
   },
 };
