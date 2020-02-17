c122daf14de6bdc0ebed48f8aa0e45b887dc1269
Kang Jie Tan <kangjietan@gmail.com>
2020-02-16 19:37:01 -0800

    Install Faker

--- cassandra.sql
+++ cassandra.sql
@@ -4,44 +4,19 @@ CREATE TABLE hotels (
   id int,
   hotel_name text,
   views int,
-  availibility_start_date date,
-  availibility_end_date date,
   1_person_max_rooms int,
   2_person_max_rooms int,
   3_person_max_rooms int,
   4_person_max_rooms int,
-);
-
-CREATE TABLE 1_person_room (
-  id int,
   site_name text,
   1_person_room_price int,
   1_person_room_discounted_price int,
-  hotel_id int,
-);
-
-CREATE TABLE 2_person_room (
-  id int,
-  site_name text,
   2_person_room_price int,
   2_person_room_discounted_price int,
-  hotel_id int,
-);
-
-CREATE TABLE 3_person_room (
-  id int,
-  site_name text,
   3_person_room_price int,
   3_person_room_discounted_price int,
-  hotel_id int,
-);
-
-CREATE TABLE 4_person_room (
-  id int,
-  site_name text,
-  3_person_room_price int,
-  3_person_room_discounted_price int,
-  hotel_id int,
+  4_person_room_price int,
+  4_person_room_discounted_price int,
 );
 
 CREATE TABLE bookings (
